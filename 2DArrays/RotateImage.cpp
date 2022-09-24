5bcc4db1931f9c50d5fc81869f297f5971c1044b
Taranjeet Singh <66739873+TARANjee@users.noreply.github.com>
2022-09-24 23:50:30 +0530

    Update RotateImage.cpp

--- 2DArrays/RotateImage.cpp
+++ 2DArrays/RotateImage.cpp
@@ -4,15 +4,36 @@ int main()
 {
     int arr[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
     int row = 3, col = 3;
-    
+    int start = 0;
+    int end = col - 1;
 
-    cout << "OUTPUT" << endl;
-
+    cout << "Input" << endl;
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    //Transpose of matrix
     for (int i = 0; i < row; i++)
     {
         for (int j = 0; j < i; j++)
         {
             swap(arr[i][j], arr[j][i]);
+        }
+    }
+    //Reverse the array
+    for (int i = 0; i < row; i++)
+    {
+        start = 0;
+        end = col - 1;
+        while (start < end)
+        {
+            swap(arr[i][start], arr[i][end]);
+            start++;
+            end--;
         }
     }
     cout << "Output" << endl;
