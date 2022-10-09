e0e049f14d8f669cd280d062066baeba8b29ee4c
Taranjeet Singh <66739873+TARANjee@users.noreply.github.com>
2022-10-09 23:25:31 +0530

    pointer

--- Pointer/playWithArray.cpp
+++ Pointer/playWithArray.cpp
@@ -3,5 +3,15 @@ using namespace std;
 
 int main()
 {
-    
+    int arr[3] = {4,6,8};
+    cout << "address of first element: " << arr << endl;
+    cout << "value of arr[0]: " << arr[0] << endl;
+    cout << "address of arr[0]: " << &arr[0] << endl;
+    cout << "*arr: " << *arr << endl;
+    cout << "4 + 1:" << *arr + 1<< endl;
+    cout << "6th : " << *(arr+1)<< endl;
+    cout << "*(i+arr): " << 2[arr]<< endl;
+    char temp='g';
+    char *ch=&temp;
+    cout<<ch<<endl;
 }
